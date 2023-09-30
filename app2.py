5ff84fd1f36630dea5bb06f6c6c856c7cc2a4f16
Gbemileke <macbook@Gbemilekes-MacBook-Pro.local>
2023-09-30 02:37:45 +0100

    appdictfix

--- app2.py
+++ app2.py
@@ -8,7 +8,7 @@ st.title('Love Island Twitter Sentiment Prediction')
 
 author.properties.friends = st.number_input('followers', 0, 3000)
 author.properties.status_count = st.number_input('statuscount', 0, 3000)
-author.properties.verified = st.selectbox('verified', options=['Yes','No'])
+author.properties.verified = st.selectbox('verified', options=['True','False'])
 content.body = st.text_area('Tweet to analyze')
 location.country = st.selectbox('country', options=['GG','GB','JE','IM'])
 properties.sentiment = st.selectbox('sentiment', options=['-1.','0.','1.'])
@@ -16,13 +16,12 @@ location.latitude = st.number_input('latitude degrees', -90, 90)
 
 
 input_data_dict = {
-    'author.properties.friends': followers,
-    'author.properties.status_count': statuscount,
-    'author.properties.verified': verified,
-    'content.body': content.body,
-    'location.country': location.country,
-    'properties.sentiment': properties.sentiment,
-    'location.latitude': location.latitude
+    'author.properties.friends': 2345,
+    'author.properties.status_count': 135,
+    'author.properties.verified': True,
+    'content.body': #LoveIsland is the biggest show in Europe right now!!,
+    'location.country': GB,
+    'location.latitude': 53.3887
 }
 
 st.write(input_data_dict)
